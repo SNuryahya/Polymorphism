ce401296ae3f74ec62af2e4d20bc455dec80ce4b
SNuryahya <stya58@gmail.com>
2020-05-06 05:06:31 +0700

    Class Program dan Strukturnya

---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorpishim_Printer
+{
+    public class PrinterWindows
+    {
+        public String merk { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine(" MERK display dimension : UKURAN");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("MERK Printer Printing ....");
+        }
+    }
+}
